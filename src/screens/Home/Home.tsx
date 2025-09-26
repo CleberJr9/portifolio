780f87c2c102a32749b1570be95e805f8736957c
CleberJr9 <clebergoesjr@gmail.com>
2025-09-26 20:21:28 -0300

    correção margem foto

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -6,16 +6,14 @@ import { ExperienciesSection } from "./sections/ExperienciesSection/Experiencies
 import { FeaturedProjectsSection } from "./sections/FeaturedProjectsSection/FeaturedProjectsSection";
 
 import { HeroSection } from "./sections/HeroSection/HeroSection";
-import { NavigationSection } from "./sections/NavigationSection/NavigationSection";
+
 import { ProjectDetailsSection } from "./sections/ProjectDetailsSection/ProjectDetailsSection";
 
 export const Home = (): JSX.Element => {
   return (
     <main className="bg-neutral-950 w-full min-h-screen">
       <div className="bg-neutralblack w-full max-w-[1440px] mx-auto relative">
-        <header className="relative w-full">
-          <NavigationSection />
-        </header>
+       
 
         <section className="relative w-full">
           <div className="flex flex-col lg:flex-row items-start justify-between px-4 sm:px-8 lg:px-[108px] pt-8 sm:pt-16 lg:pt-[126px] pb-8 sm:pb-16 lg:pb-[100px] gap-8 lg:gap-8">
@@ -24,7 +22,7 @@ export const Home = (): JSX.Element => {
             </div>
             <div className="w-full max-w-[400px] sm:max-w-[500px] lg:w-[600px] h-[400px] sm:h-[500px] lg:h-[700px] bg-neutraloffwhite rounded-2xl lg:ml-8 flex-shrink-0 mx-auto lg:mx-0">
               <img
-                className="w-full h-[363px] sm:h-[583px] lg:h-[700px]  object-cover rounded-2xl"
+                className="w-full h-[400px] sm:h-[583px] lg:h-[700px]  object-cover rounded-2xl"
                 alt="image-perfil"
                 src="https://5e23f827b0e7d9f8c7951dd4d0f08c66.cdn.bubble.io/f1757983327413x570166017478532800/WhatsApp%20Image%202025-09-15%20at%2021.41.34_efeb8385.jpg?_gl=1*xo2arl*_gcl_au*MTQ4MjMxNTE5NC4xNzUyMDYyNzU1*_ga*MTYzNDIwNjQxMi4xNzM0MzQ5MzMz*_ga_BFPVR2DEE2*czE3NTc5MzI1OTEkbzIxMCRnMSR0MTc1Nzk4MjY4OCRqNjAkbDAkaDA"
               />
